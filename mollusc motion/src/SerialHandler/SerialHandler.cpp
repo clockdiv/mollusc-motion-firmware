c02a44762f4273f3234fcfa6de26fa81d2247ddd
clockdiv <social@julianjungel.de>
2024-02-04 16:18:57 +0100

    SerialHandler

--- mollusc motion/src/SerialHandler/SerialHandler.cpp
+++ mollusc motion/src/SerialHandler/SerialHandler.cpp
@@ -29,42 +29,42 @@ void SerialHandler::comma_separated_string_to_long_array(String s, long longarra
 
 void SerialHandler::receive()
 {
-    if (Serial.available() > 0)
-    {
-        // current_millis_serial_received = millis();
+    //     if (Serial.available() > 0)
+    //     {
+    //         // current_millis_serial_received = millis();
 
-        // Read the target Positions as CSV-String from Serial
-        String incomingString = Serial.readStringUntil('\n');
+    //         // Read the target Positions as CSV-String from Serial
+    //         String incomingString = Serial.readStringUntil('\n');
 
-        if (incomingString == "MANUAL")
-        {
-            set_state(MANUAL);
-        }
-        else if (incomingString == "RUNNING")
-        {
-            set_state(RUNNING);
-        }
-        else
-        {
-            long targetPositions[14];
-            comma_separated_string_to_long_array(incomingString, targetPositions);
+    //         if (incomingString == "MANUAL")
+    //         {
+    //             set_state(MANUAL);
+    //         }
+    //         else if (incomingString == "RUNNING")
+    //         {
+    //             set_state(RUNNING);
+    //         }
+    //         else
+    //         {
+    //             long targetPositions[14];
+    //             comma_separated_string_to_long_array(incomingString, targetPositions);
 
-            // TODO enable again!
-            setNewStepperPositions();
-            setNewDynamixelPositions();
+    //             // TODO enable again!
+    //             setNewStepperPositions();
+    //             setNewDynamixelPositions();
 
-            if (state == RUNNING)
-            {
-                // TODO enable again!
-                float speed_stepper_1 = stepper_1_speed_filtered.filter(fps * float(stepper_1.distanceToGo()) * 0.1f);
-                stepper_1.setSpeed(speed_stepper_1);
-                // TODO enable again!
-                float speed_stepper_2 = stepper_2_speed_filtered.filter(fps * float(stepper_2.distanceToGo()) * 0.1f);
-                stepper_2.setSpeed(speed_stepper_2);
-                // TODO enable again!
-                float speed_stepper_3 = stepper_3_speed_filtered.filter(fps * float(stepper_3.distanceToGo()) * 0.1f);
-                stepper_3.setSpeed(speed_stepper_3);
-            }
-        }
-    }
+    //             if (state == RUNNING)
+    //             {
+    //                 // TODO enable again!
+    //                 float speed_stepper_1 = stepper_1_speed_filtered.filter(fps * float(stepper_1.distanceToGo()) * 0.1f);
+    //                 stepper_1.setSpeed(speed_stepper_1);
+    //                 // TODO enable again!
+    //                 float speed_stepper_2 = stepper_2_speed_filtered.filter(fps * float(stepper_2.distanceToGo()) * 0.1f);
+    //                 stepper_2.setSpeed(speed_stepper_2);
+    //                 // TODO enable again!
+    //                 float speed_stepper_3 = stepper_3_speed_filtered.filter(fps * float(stepper_3.distanceToGo()) * 0.1f);
+    //                 stepper_3.setSpeed(speed_stepper_3);
+    //             }
+    //         }
+    //     }
 }
