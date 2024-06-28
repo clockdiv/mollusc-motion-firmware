53753355ebcf83329a2febc2ebffb9d392736864
clockdiv <social@julianjungel.de>
2024-06-28 17:37:28 +0200

    code formatting

--- mollusc motion/src/Settings.cpp
+++ mollusc motion/src/Settings.cpp
@@ -3,19 +3,20 @@
 Settings Setting;
 
 const int Settings::chipSelect = BUILTIN_SDCARD;
-const char* Settings::filename = "settings.json";
+const char *Settings::filename = "settings.json";
 
 JsonDocument Settings::jsonDoc;
 
 int Settings::setting1;
-const char* Settings::setting2;
+const char *Settings::setting2;
 bool Settings::setting3;
-
 
 Settings::Settings() {}
 
-void Settings::initSD() {
-  if (!SD.begin(chipSelect)) {
+void Settings::initSD()
+{
+  if (!SD.begin(chipSelect))
+  {
     Serial.println("SD card initialization failed!");
     return;
   }
@@ -23,36 +24,46 @@ void Settings::initSD() {
   read();
 }
 
-void Settings::write() {
+void Settings::write()
+{
 
   jsonDoc["setting1"] = setting1;
   jsonDoc["setting2"] = setting2;
   jsonDoc["setting3"] = setting3;
 
-  if (SD.exists(filename)) {
+  if (SD.exists(filename))
+  {
     SD.remove(filename);
   }
 
   File file = SD.open(filename, FILE_WRITE);
-  if (file) {
+  if (file)
+  {
     serializeJson(jsonDoc, file);
     file.close();
     Serial.printf("Settings saved to %s\n", filename);
-  } else {
+  }
+  else
+  {
     Serial.println("Failed to open file for writing.");
   }
 }
 
-void Settings::read() {
+void Settings::read()
+{
   File file = SD.open(filename, FILE_WRITE);
   file = SD.open(filename);
-  if (file) {
+  if (file)
+  {
     JsonDocument readDoc;
     DeserializationError error = deserializeJson(readDoc, file);
-    if (error) {
+    if (error)
+    {
       Serial.printf("Failed to read file %s, error: ", filename);
       Serial.println(error.c_str());
-    } else {
+    }
+    else
+    {
       setting1 = readDoc["setting1"];
       setting2 = readDoc["setting2"];
       setting3 = readDoc["setting3"];
@@ -65,7 +76,9 @@ void Settings::read() {
       Serial.println(setting3);
     }
     file.close();
-  } else {
+  }
+  else
+  {
     Serial.println("Failed to open file for reading.");
   }
 }
